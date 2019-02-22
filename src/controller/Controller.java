14963efa12ea7be1fefbd1ff6704ea50592b0eb3
VAU0623 <VAU0623@ad.uniandes.edu.co>
2019-02-22 13:53:44 -0500

    un avance, aun en preparaciones....esta largo

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -1,17 +1,34 @@
 package controller;
 
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Scanner;
+
+
+
 import model.data_structures.*;
 import model.util.Sort;
 import model.vo.VOMovingViolation;
 import view.MovingViolationsManagerView;
 
-@SuppressWarnings("unused")
+
 public class Controller {
 
 	private MovingViolationsManagerView view;
 	
 	// TODO Definir las estructuras de datos para cargar las infracciones del periodo definido
+	
+	/**
+	 * Cola donde se van a cargar los datos de los archivos
+	 */
+	private IQueue<VOMovingViolation> movingViolationsQueue;
+
+	/**
+	 * Pila donde se van a cargar los datos de los archivos
+	 */
+	private IStack<VOMovingViolation> movingViolationsStack;
+
 	
 	// Muestra obtenida de los datos cargados 
 	Comparable<VOMovingViolation> [ ] muestra;
@@ -19,10 +36,13 @@ public class Controller {
 	// Copia de la muestra de datos a ordenar 
 	Comparable<VOMovingViolation> [ ] muestraCopia;
 
-	public Controller() {
-		view = new MovingViolationsManagerView();
-		
+	//CONSTRUCTOR
+	public Controller() 
+	{
 		//TODO inicializar las estructuras de datos para la carga de informacion de archivos
+		view = new MovingViolationsManagerView();
+		movingViolationsQueue = null;
+		movingViolationsStack = null;	
 	}
 
 	/**
@@ -31,11 +51,57 @@ public class Controller {
 	 * A partir de estos datos se obtendran muestras para evaluar los algoritmos de ordenamiento
 	 * @return numero de infracciones leidas 
 	 */
-	public int loadMovingViolations() {
-		// TODO Los datos de los archivos deben guardarse en la Estructura de Datos definida
-		
-		return 0;
+	public void loadMovingViolations(int num) {
+		movingViolationsQueue=new Queue<VOMovingViolation>();
+		movingViolationsStack= new Stack<VOMovingViolation>();
+		String[] nombresArchivos=new String[12];
+		nombresArchivos[0]="."+File.separator+"data"+File.separator+"Moving_Violations_Issued_in_January_2018_ordered.csv";
+		nombresArchivos[1]="."+File.separator+"data"+File.separator+"Moving_Violations_Issued_in_February_2018_ordered.csv";
+		nombresArchivos[2]="."+File.separator+"data"+File.separator+"Moving_Violations_Issued_in_March_2018.csv";
+		nombresArchivos[3]="."+File.separator+"data"+File.separator+"Moving_Violations_Issued_in_April_2018.csv";
+		nombresArchivos[4]="."+File.separator+"data"+File.separator+"Moving_Violations_Issued_in_May_2018.csv";
+		nombresArchivos[5]="."+File.separator+"data"+File.separator+"Moving_Violations_Issued_in_June_2018.csv";
+		nombresArchivos[6]="."+File.separator+"data"+File.separator+"Moving_Violations_Issued_in_July_2018.csv";
+		nombresArchivos[7]="."+File.separator+"data"+File.separator+"Moving_Violations_Issued_in_August_2018.csv";
+		nombresArchivos[8]="."+File.separator+"data"+File.separator+"Moving_Violations_Issued_in_September_2018.csv";
+		nombresArchivos[9]="."+File.separator+"data"+File.separator+"Moving_Violations_Issued_in_October_2018.csv";
+		nombresArchivos[10]="."+File.separator+"data"+File.separator+"Moving_Violations_Issued_in_November_2018.csv";
+		nombresArchivos[11]="."+File.separator+"data"+File.separator+"Moving_Violations_Issued_in_December_2018.csv";
+		CSVReader reader=null;
+		int inicio=-1; 
+		if(num==1){
+			inicio=0; 
+		}else if(num==2){
+			inicio=4;
+		}else{
+			inicio=8; 
+		}
+		for(int i=inicio; i<inicio+4;i++){
+			try{
+				reader=new CSVReader(new FileReader(nombresArchivos[i]));
+				String[] linea=reader.readNext();
+				linea=reader.readNext();
+				while(linea!=null){
+					movingViolationsStack.push(new VOMovingViolation(Integer.parseInt(linea[0]), linea[2], linea[13], Double.parseDouble(linea[9]), linea[12], linea[15], linea[14], Double.parseDouble(linea[8]) ));
+					movingViolationsQueue.enqueue(new VOMovingViolation(Integer.parseInt(linea[0]), linea[2], linea[13], Double.parseDouble(linea[9]), linea[12], linea[15], linea[14], Double.parseDouble(linea[8])));
+					linea=reader.readNext();
+				}
+			}catch(Exception e){
+				e.printStackTrace();
+			}finally{
+				if(reader!=null){
+					try{
+						reader.close();
+					}catch(IOException e){
+						e.printStackTrace();	
+					}
+				}
+			}
+		}
 	}
+
+	
+
 	
 	/**
 	 * Generar una muestra aleatoria de tamaNo n de los datos leidos.
--- src/model/vo/VOMovingViolation.java
+++ src/model/vo/VOMovingViolation.java
@@ -5,75 +5,140 @@ package model.vo;
  */
 public class VOMovingViolation implements Comparable<VOMovingViolation> {
 
-	// TODO Definir los atributos de una infraccion
-	
-
 	/**
-	 * Metodo constructor
+	 * identificacion infraccion
 	 */
-	public VOMovingViolation(  )
+	private int Id;
+	/**
+	 * lugar de la infraccion
+	 */
+	private String location; 
+	/**
+	 * fecha de la infraccion
+	 */
+	private String ticketIssueDate;
+	/**
+	 * valor pagado de la infraccion
+	 */
+	private double totalpaid; 
+	/**
+	 * identificador de accidente de la infraccion
+	 */
+	private String accidenIndicator; 
+	/**
+	 * descripcion infraccion
+	 */
+	private String description; 
+	/**
+	 * codigo de violacion de la infraccion
+	 */
+	private String violationcode; 
+	/**
+	 * fineAMT
+	 */
+	private double fineAMT;
+
+
+	public VOMovingViolation(int pId, String pLocation, String pTicketIssueDate, double pTotoalpaid, String pAccidentIndicator, String pDescription, String pViolationCode, double pfineAMT){
+		Id=pId;
+		location=pLocation;
+		ticketIssueDate=pTicketIssueDate;
+		totalpaid=pTotoalpaid;
+		accidenIndicator=pAccidentIndicator;
+		description=pDescription; 		
+		violationcode=pViolationCode; 
+		fineAMT=pfineAMT;
+	}
+	
+	
+	/**
+	 * @return id - Identificador único de la infracción
+	 */
+	public int objectId() 
 	{
-		// TODO Implementar
-	}	
-	
-	/**
-	 * @return id - Identificador unico de la infraccion
-	 */
-	public int objectId() {
-		// TODO Auto-generated method stub
-		return 0;
+		// TODO 
+		return Id;
 	}	
 	
 	
 	/**
-	 * @return location - Direccion en formato de texto.
+	 * @return location - Dirección en formato de texto.
 	 */
-	public String getLocation() {
-		// TODO Auto-generated method stub
-		return "";
+	public String getLocation()
+	{
+		// TODO
+		return location; 
 	}
 
 	/**
-	 * @return date - Fecha cuando se puso la infraccion .
+	 * @return date - Fecha cuando se puso la infracción .
 	 */
-	public String getTicketIssueDate() {
-		// TODO Auto-generated method stub
-		return "";
+	public String getTicketIssueDate()
+	{
+		// TODO 
+		return ticketIssueDate;
 	}
 	
 	/**
-	 * @return totalPaid - Cuanto dinero efectivamente paga el que recibio la infraccion en USD.
+	 * @return totalPaid - Cuanto dinero efectivamente pagó el que recibió la infracción en USD.
 	 */
-	public int getTotalPaid() {
-		// TODO Auto-generated method stub
-		return 0;
+	public double getTotalPaid()
+	{
+		//TODO
+		return totalpaid;
 	}
 	
 	/**
 	 * @return accidentIndicator - Si hubo un accidente o no.
 	 */
-	public String  getAccidentIndicator() {
-		// TODO Auto-generated method stub
-		return "";
+	public String  getAccidentIndicator()
+	{
+		// TODO 
+		return accidenIndicator; 
 	}
 		
 	/**
-	 * @return description - Descripcion textual de la infraccion.
+	 * @return description - Descripción textual de la infracción.
 	 */
-	public String  getViolationDescription() {
-		// TODO Auto-generated method stub
-		return "";
+	public String  getViolationDescription()
+	{
+		// TODO 
+		return description;
+	}
+	public String getViolationCode()
+	{
+		return violationcode;
+	}
+	
+	public double getFINEAMT()
+	{
+		return fineAMT;
 	}
 
 	@Override
-	public int compareTo(VOMovingViolation o) {
-		// TODO implementar la comparacion "natural" de la clase
-		return 0;
+	public int compareTo(VOMovingViolation o)
+	{
+		// TODO
+		//argumentos de la violacion recibida por parametro
+		String fechaInfraccion = o.getTicketIssueDate();
+		int idInfraccion = o.objectId();
+		//sentencia de la comparacion
+		int comparacion = 1;
+		
+		if(ticketIssueDate.compareToIgnoreCase(fechaInfraccion)==0)
+		{
+			comparacion= idInfraccion < Id? 1:-1;
+		}
+		else
+		{
+			comparacion=ticketIssueDate.compareToIgnoreCase(fechaInfraccion);
+		}
+		return comparacion;
 	}
 	
 	public String toString()
 	{
-		// TODO Convertir objeto en String (representacion que se muestra en la consola)
-		return "-";
+		// TODO 
+		return Id+","+location+","+ticketIssueDate+","+totalpaid+","+accidenIndicator+","+description+","+violationcode+","+fineAMT;
 	}
 }
