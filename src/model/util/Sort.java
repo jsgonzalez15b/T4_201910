21f73b5894804b9eb4206d875f1f36f5b4021611
cristhian forigua diaz <cristhianforiguad@gmail.com>
2019-02-24 15:52:59 -0500

    Merge

--- src/model/util/Sort.java
+++ src/model/util/Sort.java
@@ -3,13 +3,13 @@ package model.util;
 import model.vo.VOMovingViolation;
 
 public class Sort {
-	
+
 	/**
 	 * Ordenar datos aplicando el algoritmo ShellSort
 	 * @param datos - conjunto de datos a ordenar (inicio) y conjunto de datos ordenados (final)
 	 */
 	public static void ordenarShellSort( Comparable[ ] datos ) {
-		
+
 		// TODO implementar el algoritmo ShellSort
 		int N=datos.length;
 		int j=1;
@@ -22,25 +22,59 @@ public class Sort {
 			}
 		}
 	}
-	
-	private static void merge(Comparable[] datos, Comparable[] izq, Comparable[] der) {
-		int idizq=0; 
-		int idder=0; 
-		int idcompl=0;
-		while(idizq<izq.length&&idder<der.length) {
-			if(less(izq[idizq],der[idder])) {
-				datos[idcompl]=izq[idizq];
-				idizq++; 
-			}else {
-				datos[idcompl]=der[idder];
-				idder++;
-			}
-			idcompl++; 
+
+
+
+	private static void merge(Comparable[] datos, int l, int m, int r) {
+		int j; 
+		int i; 
+		int n1=m-l+1;
+		int n2=r-m;
+		Comparable[] izquierdo=new Comparable[n1];
+		Comparable[] derecho=new Comparable[n2];
+		for(i=0;i<n1;i++) {
+			izquierdo[i]=datos[l+i];
 		}
+		for(j=0; j<n2;j++) {
+			derecho[j]=datos[m+1+j];
+		}
+		i=0; 
+		j=0;
+		int k =l;
+		while(i<n1&&j<n2) {
+			if(less(izquierdo[i], derecho[j])) {
+				datos[k]=izquierdo[i];
+				i++;
+			}else {
+				datos[k]=derecho[j];
+				j++;
+			}
+		k++;
+		}
+		while(i<n1) {
+			datos[k]=izquierdo[i];
+			i++;
+			k++;
+		}
+		while(i<n2) {
+			datos[k]=derecho[j];
+			j++;
+			k++;
+		}
+		
 	}
 	
-	
-	
+	public static void mergeSort(Comparable[]datos, int izq, int der) {
+		if(izq<der) {
+			int m=izq+(der-izq)/2;
+			mergeSort(datos,izq,m);
+			mergeSort(datos, m+1, der);
+			merge(datos, izq, m, der);
+		}
+	}
+
+
+
 	/**
 	 * Ordenar datos aplicando el algoritmo MergeSort
 	 * @param datos - conjunto de datos a ordenar (inicio) y conjunto de datos ordenados (final)
@@ -48,7 +82,8 @@ public class Sort {
 	public static void ordenarMergeSort( Comparable[ ] datos ) {
 
 		// TODO implementar el algoritmo MergeSort
-	
+		mergeSort(datos, 0, datos.length-1);
+
 	}
 
 	/**
@@ -59,7 +94,7 @@ public class Sort {
 
 		// TODO implementar el algoritmo QuickSort
 	}
-	
+
 	/**
 	 * Comparar 2 objetos usando la comparacion "natural" de su clase
 	 * @param v primer objeto de comparacion
@@ -73,7 +108,7 @@ public class Sort {
 		}
 		return false; 
 	}
-	
+
 	/**
 	 * Intercambiar los datos de las posicion i y j
 	 * @param datos contenedor de datos
@@ -86,6 +121,6 @@ public class Sort {
 		Comparable copia=datos[j];
 		datos[j]=datos[i];
 		datos[i]=copia; 
-		}
+	}
 
 }
