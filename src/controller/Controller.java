541f5e8f9114f7989573e978b985a955fa6a9794
jsgonzalez15b <47197962+jsgonzalez15b@users.noreply.github.com>
2019-02-24 13:21:41 -0500

    Update Controller.java
    punto 8

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -5,7 +5,7 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.Scanner;
 
-
+import com.opencsv.CSVReader;
 
 import model.data_structures.*;
 import model.util.Sort;
@@ -51,37 +51,22 @@ public class Controller {
 	 * A partir de estos datos se obtendran muestras para evaluar los algoritmos de ordenamiento
 	 * @return numero de infracciones leidas 
 	 */
-	public void loadMovingViolations(int num) {
+	public int loadMovingViolations() {
 		movingViolationsQueue=new Queue<VOMovingViolation>();
 		movingViolationsStack= new Stack<VOMovingViolation>();
-		String[] nombresArchivos=new String[12];
-		nombresArchivos[0]="."+File.separator+"data"+File.separator+"Moving_Violations_Issued_in_January_2018_ordered.csv";
-		nombresArchivos[1]="."+File.separator+"data"+File.separator+"Moving_Violations_Issued_in_February_2018_ordered.csv";
+		int infracciones=0; 
+		String[] nombresArchivos=new String[3];
+		nombresArchivos[0]="."+File.separator+"data"+File.separator+"Moving_Violations_Issued_in_January_2018.csv";
+		nombresArchivos[1]="."+File.separator+"data"+File.separator+"Moving_Violations_Issued_in_February_2018.csv";
 		nombresArchivos[2]="."+File.separator+"data"+File.separator+"Moving_Violations_Issued_in_March_2018.csv";
-		nombresArchivos[3]="."+File.separator+"data"+File.separator+"Moving_Violations_Issued_in_April_2018.csv";
-		nombresArchivos[4]="."+File.separator+"data"+File.separator+"Moving_Violations_Issued_in_May_2018.csv";
-		nombresArchivos[5]="."+File.separator+"data"+File.separator+"Moving_Violations_Issued_in_June_2018.csv";
-		nombresArchivos[6]="."+File.separator+"data"+File.separator+"Moving_Violations_Issued_in_July_2018.csv";
-		nombresArchivos[7]="."+File.separator+"data"+File.separator+"Moving_Violations_Issued_in_August_2018.csv";
-		nombresArchivos[8]="."+File.separator+"data"+File.separator+"Moving_Violations_Issued_in_September_2018.csv";
-		nombresArchivos[9]="."+File.separator+"data"+File.separator+"Moving_Violations_Issued_in_October_2018.csv";
-		nombresArchivos[10]="."+File.separator+"data"+File.separator+"Moving_Violations_Issued_in_November_2018.csv";
-		nombresArchivos[11]="."+File.separator+"data"+File.separator+"Moving_Violations_Issued_in_December_2018.csv";
 		CSVReader reader=null;
-		int inicio=-1; 
-		if(num==1){
-			inicio=0; 
-		}else if(num==2){
-			inicio=4;
-		}else{
-			inicio=8; 
-		}
-		for(int i=inicio; i<inicio+4;i++){
+		for(int i=0; i<nombresArchivos.length;i++){
 			try{
 				reader=new CSVReader(new FileReader(nombresArchivos[i]));
 				String[] linea=reader.readNext();
 				linea=reader.readNext();
 				while(linea!=null){
+					infracciones++;
 					movingViolationsStack.push(new VOMovingViolation(Integer.parseInt(linea[0]), linea[2], linea[13], Double.parseDouble(linea[9]), linea[12], linea[15], linea[14], Double.parseDouble(linea[8]) ));
 					movingViolationsQueue.enqueue(new VOMovingViolation(Integer.parseInt(linea[0]), linea[2], linea[13], Double.parseDouble(linea[9]), linea[12], linea[15], linea[14], Double.parseDouble(linea[8])));
 					linea=reader.readNext();
@@ -98,6 +83,7 @@ public class Controller {
 				}
 			}
 		}
+		return infracciones;
 	}
 
 	
